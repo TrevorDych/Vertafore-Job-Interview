594292df06e3c4a380c65f8d515e9174683e5b22
TrevorDych <71232688+TrevorDych@users.noreply.github.com>
2020-09-13 16:44:13 -0500

    Initial Commit

---
+++ Abbreviator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Abbreviator //Application that takes in a string and provides an abbreviation in the form of (first letter)+(count of removed letters)+(last letter) and checks if the abbreviation matches the abbreviation of any word in a dictionary
+{
+
+ 
+
+    class Program
+    {
+
+
+        static string abbreviate(string word)
+        {
+            string abb = string.Empty;
+            try
+            {
+                string first = word[0].ToString();
+                string last = word[word.Length - 1].ToString();
+
+
+                // if Word is of size 1 
+                if (word.Length < 2)
+                    abb = first+"0";
+
+                //  if Word is of size 2
+                if (word.Length == 2)
+                    abb = first +"0"+ last;
+                
+
+                
+                if (word.Length >= 3)
+                abb = (first + (word.Length - 2).ToString() + last);
+
+                
+            }
+            catch { Console.WriteLine("Abbreviation has failed, please enter a string.");}
+            return abb;
+        }
+
+
+
+        static bool dictionaryLookUp(string word)
+        {
+           bool wordIsUnique = true;
+            try
+            {
+                
+                
+                List<string> dictionary = new List<string> { "accessibility", "automatically", "internationalization", "localization" };
+
+                foreach(string item in dictionary)
+                {
+                    
+                    if(abbreviate(item) == abbreviate(word))
+                    { 
+                        wordIsUnique = false;
+                    }
+                       
+                }
+
+
+
+            } catch { Console.WriteLine("Dictionary Look up has encountered an error"); }
+            return wordIsUnique;
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string.");
+            string checkWord;
+            checkWord = Console.ReadLine();
+            Console.WriteLine("the abbreviation of the word you entered is: ");
+            Console.WriteLine(abbreviate(checkWord));
+
+
+            Console.WriteLine("Is the abbreviation unique the Dictionary?");
+
+            Console.WriteLine(dictionaryLookUp(checkWord));
+           
+
+
+        }
+    }
+
+    
+}
---
+++ Scrambler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Scrambler // Method that scambles the values of a sorted integer array
+{
+    class Program
+    {
+
+        public static void scrambler(int[] arr)
+        {
+            try
+            {
+                int arrLength = arr.Length;
+                Random rand = new Random();
+
+                for (int i = 0; i < arrLength; i++)
+                {
+                    swap(arr, i, i + rand.Next(arrLength - i)); //swaps with a random index that is further in the array than i
+                }
+            }
+            catch { Console.WriteLine("Scrambler has encountered an error."); }
+        }
+        public static void swap(int[] arr, int one, int two) //int two will be a random index from further into the array
+        {
+            int temp = arr[one];
+            arr[one] = arr[two];
+            arr[two] = temp;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 564 }; //given sorted array
+            int[] arr2 = { 100, 95, 87, 64, 55, 49, 42, 37, 34, 22, 17, 8, 2 };
+
+            scrambler(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr2[i]);
+            }
+        }
+
+
+    }
+
+}
